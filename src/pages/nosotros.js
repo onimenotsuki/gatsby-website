72a0ff1ed280daa0b864ac0fe6273bccca4a10f2
Edgar Talledos <onimenotsuki@gmail.com>
2018-04-17 10:48:37 -0500

    Creating nosotros page

---
+++ src/pages/nosotros.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const NosotrosPage = () => (
+  <div>
+    <h1>Hola desde nosotros</h1>
+    <p>Bienvenido a la página de nosotros</p>
+    <Link to="/">Regresa al inicio</Link>
+  </div>
+)
+
+export default NosotrosPage
